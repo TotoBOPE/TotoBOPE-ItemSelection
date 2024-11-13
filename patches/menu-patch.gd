8745978095ebef711354420adcaa798c843a2b21
Thomas Baleix <thomas.baleix@gmail.com>
2024-11-13 14:52:02 +0100

    log var menu patch

--- patches/menu-patch.gd
+++ patches/menu-patch.gd
@@ -1,7 +1,7 @@
 extends "./patch.gd"
 
 const CPGameOptionsMenuManager = preload("../util/CPGameOptionsMenuManager.gd")
-const STPND_CHALLENGEPACK_LOG := "TotoBOPE-ItemSelection:MenuPatch"
+const ITEM_SLCT_LOG := "TotoBOPE-ItemSelection:MenuPatch"
 
 var manager: CPGameOptionsMenuManager
 
@@ -9,21 +9,21 @@ func _init():
 	scene_name = "menu"
 
 func _apply(root: Node):
-	ModLoaderLog.debug("Apply phase started", STPND_CHALLENGEPACK_LOG)
+	ModLoaderLog.debug("Apply phase started", ITEM_SLCT_LOG)
 	self.manager = _create_custom_manager(root)
-	ModLoaderLog.debug("Custom menu created", STPND_CHALLENGEPACK_LOG)
+	ModLoaderLog.debug("Custom menu created", ITEM_SLCT_LOG)
 	
 	var menu_manager = root.get_node("standalone managers/menu manager")
 	var mod_manager = self.manager
 	
-	menu_manager.buttons[0].disconnect("is_pressed", menu_manager.Start);
-	menu_manager.buttons[0].connect("is_pressed", _on_custom_start_click)
-	ModLoaderLog.debug("Start button rebind successfull", STPND_CHALLENGEPACK_LOG)
-	ModLoaderLog.debug("Apply phase finished", STPND_CHALLENGEPACK_LOG)
+	menu_manager.buttons[22].disconnect("is_pressed", menu_manager.Start);
+	menu_manager.buttons[22].connect("is_pressed", _on_custom_start_click)
+	ModLoaderLog.debug("Start button rebind successfull", ITEM_SLCT_LOG)
+	ModLoaderLog.debug("Apply phase finished", ITEM_SLCT_LOG)
 	return true
 
 func _on_custom_start_click():
-	ModLoaderLog.debug("Trying to show mod menu", STPND_CHALLENGEPACK_LOG)
+	ModLoaderLog.debug("Trying to show mod menu", ITEM_SLCT_LOG)
 	self.manager.show()
 
 func _create_custom_manager(root: Node):
